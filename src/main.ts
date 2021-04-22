3cef1950404f22b7ba9fa09e29f96a2076dc1faa
chrisp1988 <chrisp1988@gmail.com>
2021-04-22 18:32:16 +0300

    add prime/css add init root components from @/components

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,26 @@
 import { createApp } from 'vue'
 import { LockPlugin } from '@snapshot-labs/lock/plugins/vue3'
+import upperFirst from 'lodash/upperFirst'
+import camelCase from 'lodash/camelCase'
 import options from '@/utils/auth'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import PrimeVue from 'primevue/config'
 
 const app = createApp(App)
   .use(store)
   .use(router)
-  .use(PrimeVue)
   .use(LockPlugin, options)
 
+const requireComponent = require.context('@/components', true, /[\w-]+\.vue$/)
+requireComponent.keys().forEach(fileName => {
+  const componentConfig = requireComponent(fileName)
+  const componentName = upperFirst(
+    camelCase(fileName.replace(/^\.\//, '').replace(/\.\w+$/, ''))
+  )
+  app.component(componentName, componentConfig.default || componentConfig)
+})
+
 app.mount('#app')
+
 export default app
