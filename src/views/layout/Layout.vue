a77122ced36243880379965e63dd0c9c8906c250
Anastasia Andryushina <anastasia.andryushina@crplab.ru>
2021-04-23 01:29:27 +0300

    MARS-22: Base configuration

---
+++ src/views/layout/Layout.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="d-flex flex-column">
+    <Header></Header>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Header from '@/views/layout/header/Header.vue'
+
+export default {
+  name: 'Layout',
+  components: {
+    Header
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/layout/header/Header.vue
@@ -0,0 +1,33 @@
+<template>
+<header class="d-flex">
+
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+
+</header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped lang="scss">
+  header {
+    height: 80px;
+    box-sizing: border-box;
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    background-color: rgba($white, 0.03);
+    border-bottom: 1px solid rgba($white, 0.03);
+    box-sizing: border-box;
+    backdrop-filter: blur(20px);
+
+  }
+
+</style>
