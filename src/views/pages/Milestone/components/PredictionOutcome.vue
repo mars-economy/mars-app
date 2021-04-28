a78420ccb83183674ee0da5e5b197b140c7ea74e
A-Bragus <andrewbragus.gmail.com>
2021-04-28 15:43:10 +0300

    MARS-41: Outcome. Connect wallet button - functionality added

--- src/views/pages/Milestone/components/PredictionOutcome.vue
+++ src/views/pages/Milestone/components/PredictionOutcome.vue
@@ -8,22 +8,36 @@
       <TextPair :data="outcome.profit+'%'" label="estimated profit" icon="profit" />
     </div>
     <div class="card-body p-py-3">
-      <Button label="Connect wallet" class="btn-primary btn-block"/>
+      <Button label="Connect wallet" class="btn-primary btn-block" @click="openWalletPanel($event)" />
     </div>
     <div class="card-footer-collapse">
       <PanelCollapse header="Show more" headerAlt="Show less" headerPos="right" :panelId="outcome.id" collapsed>
         <TextPair :data="outcome.stake" label="total stake" icon="coins" />
       </PanelCollapse>
     </div>
+
+    <OverlayPanel ref="walletPanel" class="wallet-panel">
+      <WalletPanel :walletData=null />
+    </OverlayPanel>
+
   </div>
 
 </template>
 
 <script>
+import WalletPanel from '@/views/layout/header/Wallet/WalletPanel'
 export default {
   name: 'PredictionOutcome',
+  components: {
+    WalletPanel
+  },
   props: {
     outcome: Object
+  },
+  methods: {
+    openWalletPanel (event) {
+      this.$refs.walletPanel.show(event)
+    }
   }
 }
 </script>
