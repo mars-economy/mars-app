d5af5ab43b339eb6a6dc1478867d4121d592650c
A-Bragus <andrewbragus.gmail.com>
2021-05-07 19:18:22 +0300

    MARS-41: fixed the percentage calculation formula for icons

--- src/components/PercentImage.vue
+++ src/components/PercentImage.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <svg width="35" height="35" viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <circle stroke-dasharray="1,3" r="15" cy="17" cx="17" fill="none"/>
-      <circle :style="{'stroke-dasharray':getStyle()}" id="marker" r="16" cy="17.58" cx="17" stroke-width="3" fill="none"/>
+      <circle stroke-dasharray="1,3" r="15" cy="17.5" cx="17.5" fill="none"/>
+      <circle :style="{'stroke-dasharray':getStyle()}" id="marker" r="15" cy="17.5" cx="17.5" stroke-width="3" fill="none"/>
       <path d="M12.2002 17.5886L16.5888 22.5257L21.8002 13.2" stroke="#FFC22E" stroke-width="2"/>
     </svg>
   </div>
@@ -13,12 +13,13 @@
 export default {
   name: 'PercentImage',
   props: {
-    value: [Number, String]
+    value: [Number, String],
   },
   methods: {
     getStyle () {
-      const dash = 100 - this.value
-      return this.value + ',' + dash
+      const path = Math.PI * 30
+      const dash = this.value * path * 0.01
+      return dash + ',' + (path - dash)
     }
   }
 }
@@ -27,6 +28,7 @@ export default {
 <style scoped lang="scss">
   circle {
     stroke: $colorPrimary;
+    stroke-linecap: butt;
   }
   #marker {
     transform: rotate(-90deg);
