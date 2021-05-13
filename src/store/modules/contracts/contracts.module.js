359c6ef1a59064326ebc32a62d8e7644909bfbfb
chrisp1988 <chrisp1988@gmail.com>
2021-05-13 13:09:32 +0300

    MARS-59 rewards

--- src/store/modules/contracts/contracts.module.js
+++ src/store/modules/contracts/contracts.module.js
@@ -7,7 +7,8 @@ const getters = {}
 
 export const CONTRACTS_ACTION_TYPES = {
   BUY_OUTCOME: 'buyOutcome',
-  GET_USER_STAKES: 'getUserStakes'
+  GET_USER_STAKES: 'getUserStakes',
+  GET_REWARDS: 'getRewards'
 }
 
 export const CONTRACTS_MUTATION_TYPES = {
@@ -52,6 +53,36 @@ const actions = {
       ]
       const predictionMarketContract = await new rootState.wallet.web3.eth.Contract(MarsPredictionMarket.abi, PREDICTION_MARKET_ADDR)
       return await predictionMarketContract.methods.getUserPredictionState().call({ from: WALLET })
+    } catch (e) {
+      console.debug(e)
+      return Promise.reject(e)
+    }
+  },
+  async [CONTRACTS_ACTION_TYPES.GET_REWARDS] ({ rootState }, data) {
+    try {
+      const [WALLET, PREDICTION_MARKET_ADDR, TOKEN_ADDRESS_FROM, OUTCOME_UUID] = [
+        rootState.wallet.account,
+        data.prediction.id,
+        data.prediction.token,
+        data.outcome.id
+      ]
+      const predictionMarketContract = await new rootState.wallet.web3.eth.Contract(MarsPredictionMarket.abi, PREDICTION_MARKET_ADDR)
+      const tokenAddress = await predictionMarketContract.methods.tokenOutcomeAddress(data.outcome.id).call()
+      const tokenContractFrom = await new rootState.wallet.web3.eth.Contract(erc20, tokenAddress)
+
+      const tokenDecimals = await tokenContractFrom.methods.decimals().call()
+      const amount = _toBN(data.reward.win, tokenDecimals).toFixed()
+
+      const rs = await checkAllowance(tokenContractFrom, WALLET, PREDICTION_MARKET_ADDR, amount)
+      if (rs) {
+        await tokenContractFrom.methods.approve(PREDICTION_MARKET_ADDR, rs).send({ from: WALLET })
+      }
+
+      await predictionMarketContract.methods.getReward()
+        .send({ from: WALLET })
+        .then(tx => {
+          Promise.resolve(tx)
+        })
     } catch (e) {
       console.debug(e)
       return Promise.reject(e)
